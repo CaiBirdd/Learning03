7c3c0561c16f282ba990012abe881ccb0bb8854b
CaiBirdd <2960693496@qq.com>
2025-10-02 22:51:43 +0800

    10.2联调完成

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import router from './router'
 import 'vant/es/notify/style/index.mjs' //vant通知样式生效
 
 const app = createApp(App)
-
+  .
+//导航守卫
+  router.beforeEach((to,from)=>{
+    if(to.path !== 'login'){
+      if(!localStorage.getItem('h5_token')){
+        return '/login'
+      }
+    }
+  })
 app.use(router)
 
 app.mount('#app')
